66990e409db6de57042c2bdce6e634f30d6d3bb0
Julia-Kopichenko <j.titorenko@gmail.com>
2023-06-18 15:38:46 +0200

    feat: add list.js solution

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,16 @@
+import { readdir, access } from 'fs/promises';
+import { FOLDER_FILES_PATH, ERROR_MSG } from './constants.js';
+
 const list = async () => {
-    // Write your code here 
+  try {
+    await access(FOLDER_FILES_PATH);
+
+    const fileNames = await readdir(FOLDER_FILES_PATH);
+
+    console.log(fileNames);
+  } catch {
+    throw new Error(ERROR_MSG);
+  }
 };
 
 await list();
